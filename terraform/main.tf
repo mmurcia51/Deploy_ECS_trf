3b968b6dcaf1659f1343cdd6db83a72786b9663c
mmurcia51 <mmurcia51@uan.edu.co>
2023-12-13 20:00:03 -0500

    prueba

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "tu_region_aws"
+}
+
+resource "aws_ecs_cluster" "grafana_cluster" {
+  name = "grafana-cluster"
+}
+
+resource "aws_ecs_task_definition" "grafana_task" {
+  // Define tu tarea ECS para Grafana
+}
+
+resource "aws_ecs_service" "grafana_service" {
+  // Define tu servicio ECS para Grafana
+}
